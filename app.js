bbc3a6ce7d91fb4076587cba4951a0457e7f8388
Taslim Raza Ansari <imtas00@gmail.com>
2022-01-03 00:10:00 +0530

    fixed cors

--- app.js
+++ app.js
@@ -8,17 +8,21 @@ const app = express();
 const users = require('./router/user-router');
 const products = require('./router/product-router');
 const carts = require('./router/cart-router');
-const orders = require('./router/order-router')
+const orders = require('./router/order-router');
+const cors = require('cors');
+
+
 app.use(express.json());
 
 dotenv.config();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
-    res.header("Access-Control-Allow-Methods", "*");
-    next();
-});
+app.use(cors());
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+//     res.header("Access-Control-Allow-Methods", "*");
+//     next();
+// });
 
 (async () => {
     const DB_URL = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
