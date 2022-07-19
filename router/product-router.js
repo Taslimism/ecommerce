8302f1a082af047d7d58f8d58605b3ab2d2dbcb2
Taslim Raza Ansari <41345027+Taslimism@users.noreply.github.com>
2022-07-20 00:46:19 +0530

    improved pagination

--- router/product-router.js
+++ router/product-router.js
@@ -30,6 +30,7 @@ router.get("/:authorName", async (req, res, next) => {
 
 router.get("/get/allbooks/allkind", async (req, res, next) => {
 	const { count, page } = req.query;
+	console.log(count, page);
 	try {
 		products = await Knuth_Book.find({})
 			.skip(count * page)
